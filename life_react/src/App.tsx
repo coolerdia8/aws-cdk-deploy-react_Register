c95d4799f36533485fbeab1bb610c2bf7a347ad2
ufoo68 <mitarou.yuutarou@gmail.com>
2020-03-27 00:34:47 +0900

    UseState

--- life_react/src/App.tsx
+++ life_react/src/App.tsx
@@ -3,6 +3,11 @@ import { Profile } from '@line/bot-sdk'
 import './App.scss'
 import Select from 'react-select'
 
+interface OptionType {
+  label: string
+  value: string
+}
+
 const options = [
   { value: 'pikachu', label: 'ピカチュウ' },
   { value: 'bulbasaur', label: 'フシギダネ' },
@@ -11,12 +16,8 @@ const options = [
 ]
 
 const App: FC = () => {
-  const [profile, setProfile] = useState<Profile>({
-    displayName: '',
-    userId: '',
-    pictureUrl: '',
-    statusMessage: '',
-  })
+  const [myName, setMyName] = useState<string>("");
+  const [storeName, setStoreName] = useState<OptionType>();
   /*
   useEffect(() => {
 
@@ -35,51 +36,33 @@ const App: FC = () => {
   }, [])
   */
 
- /*decrement() {
-  this.setState(prevState => ({
-    count: prevState.count - 1,
-  }));
-}
-*/
   return (
     <div className="App">
-      {
-        /*<div className="name">
-        {profile.displayName}
-      </div>
-      <img src={profile.pictureUrl} className="picture"/>
-      <div className="status">
-        {profile.statusMessage}
-      </div>
-      <div className="userid">
-        {profile.userId}
-      </div>*/
-      }
       <form id="send-message">
-            <p>送信したいメッセージを入れてね</p>
-            <input type="text" className="名前"  placeholder="名前を入力" />
-            {//<label>店名</label>
-            }
-            <p>店名</p>
-            <Select options={options} placeholder="店名"/>
-            <p>生年月日</p>
-            <div className="form-row">
-              <div className="form-group col-4">
-                <label>年</label>
-                <input type="text" className="form-control" id="year" />
-              </div>
-              <div className="form-group col-4">
-                <label>月</label>
-                <input type="text" className="form-control" id="month" />
-              </div>
-            <div className="form-group col-4">
-                <label>日</label>
-                <input type="text" className="form-control" id="day" />
-              </div>
-            </div>
-            <button id="send" className="btn btn-primary" >送信</button>
+        <p>送信したいメッセージを入れてね</p>
+        <input type="text" className="名前" placeholder="名前を入力" value={myName} onChange={(e) => setMyName(e.target.value) } />
+        {//<label>店名</label>
+        }
+        <p>店名</p>
+        <Select options={options} placeholder="店名" onChange={(v: any)=> setStoreName(v)} />
+        <p>生年月日</p>
+        <div className="form-row">
+          <div className="form-group col-4">
+            <label>年</label>
+            <input type="text" className="form-control" id="year" />
+          </div>
+          <div className="form-group col-4">
+            <label>月</label>
+            <input type="text" className="form-control" id="month" />
+          </div>
+          <div className="form-group col-4">
+            <label>日</label>
+            <input type="text" className="form-control" id="day" />
+          </div>
+        </div>
+        <button id="send" className="btn btn-primary" >送信</button>
 
-        </form>
+      </form>
     </div>
   );
 }
